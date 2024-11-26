1f1172099a516d084eb371f541df4362aa287cae
Siddharth Garg <sizzler.sid@gmail.com>
2024-11-26 16:12:30 +0530

    Initial commit

---
+++ src/test/java/com/devops/coach/docker_spring_boot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.coach.docker_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
